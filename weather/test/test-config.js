98286caba2ceb4cc539540116b2d1d577402c724
pduret <pierrelouis.duret@gmail.com>
2016-02-23 16:00:15 +0100

    Push2

--- weather/test/test-config.js
+++ weather/test/test-config.js
@@ -2,10 +2,22 @@ var assert = require('assert'),
     config = require('./../lib/config');
 
 
-describe('Config', function () {
-    it('has 3 props')
+var a = config("test", "test2");
 
-    it('after construction, 2 props are populated')
+describe('Config', function() {
 
-    it('has correct key types')
+    it('has 3 props', function () {
+        assert(Object.keys(a).length == 3);
+    });
+    
+    it('after construction, 2 props are populated', function() {
+        
+    });
+
+
+
+    it('has correct key types', function () {
+        assert(typeof('test') == 'string');
+    });
+    
 });
--- weather/test/test-format.js
+++ weather/test/test-format.js
@@ -1,6 +1,13 @@
+var expect    = require("chai").expect;
+
 var assert = require('assert'),
-    format = require('./../lib/format');
+    formats = require('./../lib/format');
 
 describe('format', function () {
+
+	it("should be defined", function() {
+    	expect(formats.format).to.exist;
+  	});
+
     it('is a constructor of the Format object')
 });
